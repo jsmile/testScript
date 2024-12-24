49cc418998f22a390dd7be6dc8f9225efac04895
Byeong Gui, Jeong <jsmile@naver.com>
2024-12-24 19:56:17 +0900

    Async, Await

--- promise/ajax.js
+++ promise/ajax.js
@@ -158,9 +158,132 @@ async function postData( url = "", data = {} ) {
 /*
    Async, Await : 비동기화된 Promise 객체를 반환한다, ES7( 2017 )
 
+   - async function : 암시적으로 Promise를 사용하여 결과를 반환, 내용에 await 가 포함될 수 있음.
    - await Promise 는 Promise.then( (res) = {} ) 와 동일함.
    - 성공( resolve )과 실패( reject )를 가지고 있는 callback 함수를 포함한 Promise 객체를 반환한다.
    - 기본은 성공( resolve )을 반환하는데 함수 내에서 throw 를 해줄 때에만 실패( reject )를 반환한다.
    - Micro Task Queue 이용
    - IE 지원하지 않음.
 */
+
+async function foo() {
+   return 1;
+ }
+// 는 아래와 동일함.
+function foo() {
+   return Promise.resolve(1);
+} 
+
+
+async function foo() {
+   await 1;
+ }
+// 는 아래와 동일함.
+function foo() {
+   return Promise.resolve(1).then(() => undefined);
+} 
+
+
+async function add( x, y ) {
+   
+   if( typeof x !== 'number' || typeof y !== 'number' ) {
+      throw 'x, y 는 숫자여야 합니다.';   // 실패( reject )를 반환      // .catch() 에서 처리하기 위해 throw 사용
+   }
+
+   return x + y;
+}
+
+add( 1, 2 )
+   .then( ( result ) => {
+      console.log( 'add( 1, 2 ) : ', result );
+   } )
+   .catch( 
+      ( error ) => {
+         console.log( 'add( 1, 2 ) error: ', error );
+      }
+   );
+
+add( 'c', 2 )
+   .then( ( result ) => {
+      console.log( 'add( c, 2 ) : ', result );
+   } )
+   .catch( 
+      ( error ) => {
+         console.log( 'add( c, 2 ) error: ', error );
+      }
+   );
+
+
+// async ~ await 사용 예제
+var resolveAfter2Seconds = function () {
+   console.log("시작...slow promise");
+   return new Promise((resolve) => {
+     setTimeout(function () {
+       resolve(20);
+       console.log("완료...slow promise");
+     }, 2000);
+   });
+ };
+ 
+ var resolveAfter1Second = function () {
+   console.log("시작...fast promise");
+   return new Promise((resolve) => {
+     setTimeout(function () {
+       resolve(10);
+       console.log("완료...fast promise");
+     }, 1000);
+   });
+ };
+
+ // 실행과 작업종료 모두 await 순서대로 진행됨.
+ var sequentialStart = async function () {
+   console.log("### SEQUENTIAL START ###");
+ 
+   const slow = await resolveAfter2Seconds();
+   console.log('sequentialStart: ', slow);
+ 
+   const fast = await resolveAfter1Second();
+   console.log('sequentialStart: ', fast);
+ };
+
+ // 순차적으로 실행되고, 먼저 작업 종료와 상관없이 await 순서로 출력함.
+ var concurrentStart = async function () {
+   console.log("### CONCURRENT START with await ###");
+
+   // 순차적 실행, 작업 소요 시간에 따른 종료
+   const slow = resolveAfter2Seconds();  // starts timer immediately
+   const fast = resolveAfter1Second();
+   // 그러나 await 순서에 따른 출력
+   console.log( 'concurrentStart ', await slow );
+   console.log( 'concurrentStart: ', await fast );  // waits for slow to finish, even though fast is already done!
+ };
+ 
+ // 순차적으로 실행되고, 모든 작업들이 종료된 후 결과를 반환함.
+ var stillConcurrent = function () {
+   console.log("### CONCURRENT START with Promise.all ###");
+
+   // 모든 것들이 종료되고 난 다음에 결과들을을 반환
+   Promise.all([ resolveAfter2Seconds(), resolveAfter1Second() ])   
+      .then(
+         (messages) => {
+            console.log( 'stillConcurrent: ', messages[0] ); // slow
+            console.log( 'stillConcurrent: ', messages[1] ); // fast
+         },
+      );
+ };
+ 
+ // 순차적으로 실행되고, 빨리 작업이 종료되는 것부터 결과를 반환함.
+ var parallel = function () {
+   console.log("### PARALLEL with Promise.then ###");
+
+   resolveAfter2Seconds().then((message) => console.log( 'parallel: ', message));
+   resolveAfter1Second().then((message) => console.log( 'parallel: ', message));
+ };
+ 
+ sequentialStart(); // after 2 seconds, logs "slow", then after 1 more second, "fast"
+ // wait above to finish
+ setTimeout(concurrentStart, 4000); // after 2 seconds, logs "slow" and then "fast"
+ // wait again
+ setTimeout(stillConcurrent, 7000); // same as concurrentStart
+ // wait again
+ setTimeout(parallel, 10000); // trully parallel: after 1 second, logs "fast", then after 1 more second, "slow"   
